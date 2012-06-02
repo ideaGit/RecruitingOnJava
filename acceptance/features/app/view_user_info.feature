9afd96fbf05d5cd5ddfc0f4400234d92e3f6462a
Heven <zhwheven@126.com>
2012-06-02 01:51:29 -0700

    #3 add cucumber test. pair:zj,qym

---
+++ acceptance/features/app/view_user_info.feature
@@ -0,0 +1,9 @@
+Feature:  As an admin
+  I want to view all the basic information in a list,
+  so that I can be clear about the candidates.
+
+  @wip
+  Scenario: admin view candidates information
+   Given a candidate submit his information
+    When I am on the candidates information page
+    Then I can see the list of candidates information
---
+++ acceptance/features/step_definitions/view_user_info_steps.rb
@@ -0,0 +1,10 @@
+When /^I am on the candidates information page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I can see the list of candidates information$/ do
+  pending # express the regexp above with the code you wish you had
+end
+Given /^a candidate submit his information$/ do
+    pending # express the regexp above with the code you wish you had
+end
