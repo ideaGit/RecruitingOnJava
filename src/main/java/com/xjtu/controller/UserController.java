426e93933691549a976eaaa0739c2b81b1dc58e1
FENG Zhichao <flankerfc@gmail.com>
2012-06-09 19:36:22 +1000

    show error page if validation fail

--- src/main/java/com/xjtu/controller/UserController.java
+++ src/main/java/com/xjtu/controller/UserController.java
@@ -9,19 +9,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 @Controller
 public class UserController {
 
     @Autowired
     private UserDao userDao;
+    private final Validator validator;
+
+    public UserController() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
 
     @Transactional
     @RequestMapping(value = "/user", method = RequestMethod.POST)
     public String index(@RequestParam("name") String name, @RequestParam("university") String university,
                         @RequestParam("phone") String phone, @RequestParam("email") String email,
                         @RequestParam("major") String major) {
-        userDao.save(new User(name, university, phone, email, major));
-        return "success";
+
+        User user = new User(name, university, phone, email, major);
+
+        if (validator.validate(user).isEmpty()) {
+            userDao.save(user);
+            return "success";
+        } else {
+            return "error";
+        }
 
     }
 
---
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,1 @@
+you have not submitted your information successfully
