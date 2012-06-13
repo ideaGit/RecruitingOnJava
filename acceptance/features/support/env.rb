0545bd3ae57d9f71a171b7c0599f150c752924cc
FENG Zhichao <flankerfc@gmail.com>
2012-06-13 13:24:42 +1000

    use headless acceptance test in ci build. feng zhichao

--- acceptance/features/support/env.rb
+++ acceptance/features/support/env.rb
@@ -6,13 +6,7 @@ end
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/cucumber'
-
-Capybara.default_driver = :selenium
-Capybara.default_wait_time = 10
-Capybara.default_selector= :css
-Capybara::Selenium::Driver::DEFAULT_OPTIONS[:resynchronize] = false
-Capybara.ignore_hidden_elements = false
-Capybara.app_host = 'http://localhost:8080'
+require 'capybara/mechanize/cucumber'
 
 PROJECT_ROOT = File.join(File.dirname(__FILE__), "../../../.")
 env = ENV['ENV'] || 'local'
@@ -20,5 +14,16 @@ puts "=== you are using #{env} environment ==="
 yaml = YAML.load_file("#{PROJECT_ROOT}/config.yml")
 
 CONFIG_SETTING = yaml[env]
+
+if env == 'ci'
+  Capybara.default_driver = :mechanize
+else
+  Capybara.default_driver = :selenium
+end
+Capybara.default_wait_time = 10
+Capybara.default_selector= :css
+Capybara::Selenium::Driver::DEFAULT_OPTIONS[:resynchronize] = false
+Capybara.ignore_hidden_elements = false
+Capybara.app_host = 'http://localhost:8080'
 
 World(Capybara)
--- ci.sh
+++ ci.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+# install ruby and gems
+
 curl -s -o use-ruby https://repository-cloudbees.forge.cloudbees.com/distributions/ci-addons/use-ruby
 RUBY_VERSION=1.8.7-p352 source ./use-ruby
 
 gem install --conservative bundler
 
 bundle check || bundle install
+
+# run buildr ci
 
 ENV=ci bundle exec buildr ci
